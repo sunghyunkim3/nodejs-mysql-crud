62d87e1e13e83994b3781f03eb21bbc3d1125f95
Sunghyun Kim <sunghyun_kim3@tmax.co.kr>
2020-04-08 10:07:15 +0900

    new

--- mysql-init/privilege.sh
+++
@@ -1,3 +0,0 @@
-PW=$([ -z "${MYSQL_ROOT_PASSWORD}" ] && printf '' || printf "%s" "-p${MYSQL_ROOT_PASSWORD}")
-mysql -uroot ${PW} -e "grant all privileges on *.* to ${MYSQL_USER}@'%'; flush privileges;"
-
